1c19d4245ca4f49da731b481e4220db7e14421d7
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-07-04 13:31:22 -0400

    editando autos

--- autos.sql
+++ autos.sql
@@ -33,3 +33,42 @@ CREATE TABLE IF NOT EXISTS public.ventas
         ON DELETE NO ACTION
         NOT VALID
 )
+-- importar tabla autos
+\copy autos FROM '/Users/mariajosevillasante/Documents/EmprendimientoDigital/M7-Introduccion_a_bases_de_datos/1-Basesdedatosrelacionales/dia2/ApoyoLectura-Basededatosrelacionales/autos.csv' csv header;
+
+-- 6- hacer cosulta con alias
+SELECT marca, modelo as "tipo", year as "año", color
+	FROM public.autos;
+
+-- hacer 2 inserciones en la tabla ventas 
+INSERT INTO public.ventas(
+	id, fecha, cliente, referencia, cantidad, id_auto)
+	VALUES (1, '2022-01-05', 'Cesar Osses', 'Compañero de trabajo', 2, 1);
+INSERT INTO public.ventas(
+	id, fecha, cliente, referencia, cantidad, id_auto)
+	VALUES (2, '2022-01-27', 'Miguel Angel', 'Amigo', 2, 3);
+
+-- realizar una suma
+SELECT SUM(cantidad) as "autos vendidos" from ventas
+
+-- agregar una columna en una tabla 
+-- ALTER TABLE table_name
+-- ADD column_name datatype;
+ALTER TABLE autos
+ADD estado text;
+
+-- insertar nuevo auto
+INSERT INTO public.autos(
+	id, marca, modelo, year, color, estado)
+	VALUES (5, 'mazda', 'cx5', 2022, 'grafito', 'nuevo')
+
+-- Actualizar info
+UPDATE public.autos
+	SET marca='CHEVROLET', modelo='AVEO' 
+	WHERE ID=2;
+
+-- agrupar y contar por marca
+SELECT count(marca), marca
+	FROM public.autos
+    group by marca;
+
