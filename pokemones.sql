28764794d3bf66dd76d0dc5e6a733df7a2ff524d
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-07-05 15:27:02 -0400

    editando pokemones

--- pokemones.sql
+++ pokemones.sql
@@ -19,3 +19,11 @@ SELECT b.nombre, fecha_captura, lugar, huevo, peso, estatura
     right join public.pokemones b
     on a.pokedex=b.pokedex
     where a.huevo='true';
+
+-- obtener los nombres de los pokemones tengan peso mayor que
+SELECT b.nombre
+	FROM public.mis_pokemones a
+    inner join public.pokemones b
+    on a.pokedex=b.pokedex
+    where a.peso>200;
+
